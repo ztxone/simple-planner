111aad24d6856a8a029efeb02fda7c154930b939
Anton Cherry <39146381+ztxone@users.noreply.github.com>
2021-09-08 17:17:36 +0200

    added json-server pkg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,17 @@
 <script>
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message))
   },
 }
 </script>
